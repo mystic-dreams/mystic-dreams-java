a8ca54527fb5e08fab460855eb6fbbc9af295119
Phoenix <terencenjr.dev@gmail.com>
2023-09-10 17:41:46 +0800

    Added missing parsing for basic attack

--- src/main/utility/SkillParser.java
+++ src/main/utility/SkillParser.java
@@ -3,6 +3,7 @@ package main.utility;
 import main.exceptions.InvalidSkillException;
 import main.exceptions.LevelExceededException;
 import main.skills.Skill;
+import main.skills.attack.BasicAttack;
 import main.skills.attack.magician.ManaBolt;
 import main.skills.buff.magician.ManaArmor;
 import main.skills.passive.magician.ManaWell;
@@ -12,20 +13,13 @@ import static main.skills.SkillNames.*;
 public class SkillParser {
     public static Skill parseSkill(String skillName, int level) throws LevelExceededException, InvalidSkillException {
 
-        Skill skill;
-        switch (skillName) {
-            case MANA_WELL_NAME:
-                skill = ManaWell.invoke(level);
-                break;
-            case MANA_ARMOR_NAME:
-                skill = ManaArmor.invoke(level);
-                break;
-            case MANA_BOLT_NAME:
-                skill = ManaBolt.invoke(level);
-                break;
-            default:
-                throw new InvalidSkillException(skillName);
-        }
+        Skill skill = switch (skillName) {
+            case MANA_WELL_NAME -> ManaWell.invoke(level);
+            case MANA_ARMOR_NAME -> ManaArmor.invoke(level);
+            case MANA_BOLT_NAME -> ManaBolt.invoke(level);
+            case BASIC_ATTACK_NAME -> new BasicAttack();
+            default -> throw new InvalidSkillException(skillName);
+        };
 
         return skill;
     }
